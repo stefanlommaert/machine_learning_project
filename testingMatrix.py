32cfbef2cfd1b9e3fa9d96b30819880a722aa6ae
gandhalf <stefan.lommaert@student.kuleuven.be>
2022-03-08 10:03:33 +0100

    use pip install -r requirements.txt to install requirements, also added sources document

--- testingMatrix.py
+++ testingMatrix.py
@@ -3,6 +3,8 @@ import pyspiel
 import numpy as np
 from open_spiel.python.egt import dynamics
 from open_spiel.python.egt.utils import game_payoffs_array
+import matplotlib.pyplot as plt
+
 
 
 
@@ -20,7 +22,26 @@ chance_distribution = np.array([0.49, 0.51])
 learning_rate = 0.1
 for i in range(50):
     chance_distribution += learning_rate*dyn(chance_distribution)
-    print(chance_distribution)
+    # print(chance_distribution)
+
+
+X, Y = np.meshgrid(np.linspace(0.0, 1.0, 50), np.linspace(0.0, 1.0, 50))
+u, v = np.zeros_like(X), np.zeros_like(X)
+NI, NJ = X.shape
+for i in range(NI):
+    for j in range(NJ):
+        x, y = X[i, j], Y[i, j]
+        chance_distribution = np.array([x, 1-x])
+        print((x,y),dyn(chance_distribution))
+        # print((x,y))
+        u[i,j] = dyn(chance_distribution)[0]
+        v[i,j] = dyn(chance_distribution)[1]
+plt.streamplot(X, Y, u, v)
+plt.axis('square')
+plt.axis([0, 1, 0, 1])
+plt.show()
+
+
 
 
 # game = pyspiel.load_matrix_game("matrix_rps")
